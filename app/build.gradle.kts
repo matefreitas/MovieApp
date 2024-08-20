ac33dc3f390e6597c38b81080b9de3b79a1ef17a
Mateus <mateus.silvafreitas27@gmail.com>
2024-08-19 23:01:04 -0300

    dependecias do projeto e api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
+
+val apiKeyPropertiesFile = rootProject.file("apiKey.properties")
+val apiKeyProperties = Properties()
+apiKeyProperties.load(FileInputStream(apiKeyPropertiesFile))
 
 android {
     namespace = "com.example.movieapp"
@@ -13,6 +22,10 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        buildConfigField("String", "API_KEY", apiKeyProperties["API_KEY"].toString())
+        buildConfigField("String", "BASE_URL", apiKeyProperties["BASE_URL"].toString())
+        buildConfigField("String", "BASE_URL_IMAGE", apiKeyProperties["BASE_URL_IMAGE"].toString())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -38,6 +51,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -59,6 +73,33 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.coil.compose)
+    implementation(libs.timber)
+    implementation(libs.data.store)
+    implementation(libs.splash.screen)
+    implementation(libs.gson)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.material.icons)
+    implementation(libs.flow.layout)
+    implementation(libs.navigation)
+    implementation(libs.paging.runtime.ktx)
+    implementation(libs.paging.compose)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.hilt)
+    implementation(libs.hilt.compose)
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+
+    annotationProcessor(libs.room.compiler)
+
+    ksp(libs.hilt.compiler)
+    ksp(libs.hilt.compose.compiler)
+    ksp(libs.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +107,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
