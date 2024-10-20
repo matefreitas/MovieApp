9649ab8d3d58aec00d1d860298446bf4be3ecf08
Mateus <mateus.silvafreitas27@gmail.com>
2024-10-20 19:32:44 -0300

    movie viewmodel

--- app/src/main/java/com/example/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
+++ app/src/main/java/com/example/movieapp/movie_popular_feature/presentation/components/MovieContent.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
