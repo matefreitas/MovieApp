24ef6d45bff70d53437709a80e8d6135bc0b4959
Mateus <mateus.silvafreitas27@gmail.com>
2024-11-18 20:13:45 -0300

    Usecase insert favorite

---
+++ app/src/main/java/com/example/movieapp/move_favorite_feature/data/usecase/AddMovieFavoriteUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.movieapp.move_favorite_feature.data.usecase
+
+import com.example.movieapp.core.domain.model.Movie
+import com.example.movieapp.core.util.ResultData
+import com.example.movieapp.move_favorite_feature.domain.repository.MovieFavoriteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+interface AddMovieFavoriteUseCase{
+    suspend operator fun invoke(params: Params): Flow<ResultData<Unit>>
+    data class Params(val movie: Movie)
+}
+
+class AddMovieFavoriteUseCaseImpl @Inject constructor(
+    private val movieFavoriteRepository: MovieFavoriteRepository
+) : AddMovieFavoriteUseCase{
+    override suspend fun invoke(params: AddMovieFavoriteUseCase.Params): Flow<ResultData<Unit>> {
+        return flow {
+            val insert = movieFavoriteRepository.insert(params.movie)
+            emit(ResultData.Success(insert))
+        }.flowOn(Dispatchers.IO)
+    }
+}
