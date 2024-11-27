fbdde15d55e6dc5b9e32008245d699319fa7aa03
Mateus <mateus.silvafreitas27@gmail.com>
2024-11-27 20:46:06 -0300

    empty list mensage

--- app/src/main/java/com/example/movieapp/move_favorite_feature/presentation/components/MovieFavoriteContent.kt
+++ app/src/main/java/com/example/movieapp/move_favorite_feature/presentation/components/MovieFavoriteContent.kt
@@ -1,16 +1,30 @@
 package com.example.movieapp.move_favorite_feature.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.movieapp.R
 import com.example.movieapp.core.domain.model.Movie
+import com.example.movieapp.ui.theme.black
+import com.example.movieapp.ui.theme.white
+import com.example.movieapp.ui.theme.yellow
 
 @Composable
 fun MovieFavoriteContent(
@@ -19,7 +33,17 @@ fun MovieFavoriteContent(
     movies: List<Movie>,
     onClick: (id: Int) -> Unit
 ) {
-    Box(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (movies.isEmpty()) {
+            Text(
+                text = stringResource(R.string.favorite_movies_empty),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
--- app/src/main/java/com/example/movieapp/movie_popular_feature/presentation/MoviePopularScreen.kt
+++ app/src/main/java/com/example/movieapp/movie_popular_feature/presentation/MoviePopularScreen.kt
@@ -2,17 +2,13 @@ package com.example.movieapp.movie_popular_feature.presentation
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.movieapp.R
 import com.example.movieapp.core.presentation.components.commom.MovieAppBar
 import com.example.movieapp.core.util.UtilFunctions
 import com.example.movieapp.movie_popular_feature.presentation.components.MovieContent
 import com.example.movieapp.movie_popular_feature.presentation.state.MoviePopularState
-import com.example.movieapp.ui.theme.white
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
