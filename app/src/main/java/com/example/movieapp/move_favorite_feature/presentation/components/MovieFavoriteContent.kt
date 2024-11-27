ac9eb24e1bed745751263f00d8ef4bb321e9c70f
Mateus <mateus.silvafreitas27@gmail.com>
2024-11-27 20:46:28 -0300

    empty list mensage

--- app/src/main/java/com/example/movieapp/move_favorite_feature/presentation/components/MovieFavoriteContent.kt
+++ app/src/main/java/com/example/movieapp/move_favorite_feature/presentation/components/MovieFavoriteContent.kt
@@ -1,11 +1,8 @@
 package com.example.movieapp.move_favorite_feature.presentation.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,15 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.movieapp.R
 import com.example.movieapp.core.domain.model.Movie
-import com.example.movieapp.ui.theme.black
-import com.example.movieapp.ui.theme.white
-import com.example.movieapp.ui.theme.yellow
 
 @Composable
 fun MovieFavoriteContent(
