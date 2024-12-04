18c92cecd1f61881633e058adc7507b0119021ef
Mateus <mateus.silvafreitas27@gmail.com>
2024-12-04 20:01:13 -0300

    test movies order by id

--- app/src/androidTest/java/com/example/movieapp/core/data/local/dao/MovieDaoTest.kt
+++ app/src/androidTest/java/com/example/movieapp/core/data/local/dao/MovieDaoTest.kt
@@ -3,6 +3,7 @@ package com.example.movieapp.core.data.local.dao
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
 import com.example.movieapp.core.data.local.MovieDatabase
+import com.example.movieapp.core.data.local.entity.MovieEntity
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -46,5 +47,24 @@ class MovieDaoTest {
         val movies = movieDao.getMovies().first()
 
         assertThat(movies.size).isEqualTo(0)
+    }
+
+    @Test
+    fun getMovies_orderById() = runTest{
+        val moviesEntitys = listOf(
+            MovieEntity(movieId = 1, title = "Homem de Ferro 1", imageUrl = "Url1"),
+            MovieEntity(movieId = 4, title = "Homem de Ferro 2", imageUrl = "Url1"),
+            MovieEntity(movieId = 2, title = "Homem de Ferro 3", imageUrl = "Url1"),
+            MovieEntity(movieId = 3, title = "Homem de Ferro 4", imageUrl = "Url1")
+        )
+        movieDao.insertMovies(moviesEntitys)
+
+        val movies = movieDao.getMovies().first()
+
+        assertThat(movies.size).isEqualTo(4)
+        assertThat(movies[0].movieId).isEqualTo(1)
+        assertThat(movies[1].movieId).isEqualTo(2)
+        assertThat(movies[2].movieId).isEqualTo(3)
+        assertThat(movies[3].movieId).isEqualTo(4)
     }
 }
