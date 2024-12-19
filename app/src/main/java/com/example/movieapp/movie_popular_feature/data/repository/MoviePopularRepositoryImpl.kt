30ef7ab15c8cb89a51c03bf068413336c37eb32e
mateus <mateus.silvafreitas@gmail.com>
2024-12-19 13:43:52 -0300

    resolve empty imports

--- app/src/main/java/com/example/movieapp/movie_popular_feature/data/repository/MoviePopularRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/movie_popular_feature/data/repository/MoviePopularRepositoryImpl.kt
@@ -1,14 +1,10 @@
 package com.example.movieapp.movie_popular_feature.data.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import com.example.movieapp.core.domain.model.Movie
 import com.example.movieapp.core.paging.MoviePagingSource
 import com.example.movieapp.movie_popular_feature.domain.repository.MoviePopularRepository
 import com.example.movieapp.movie_popular_feature.domain.source.MoviePopularRemoteDataSource
-import kotlinx.coroutines.flow.Flow
 
 class MoviePopularRepositoryImpl constructor(
     private val remoteDataSource: MoviePopularRemoteDataSource
--- app/src/main/java/com/example/movieapp/movie_popular_feature/domain/repository/MoviePopularRepository.kt
+++ app/src/main/java/com/example/movieapp/movie_popular_feature/domain/repository/MoviePopularRepository.kt
@@ -1,10 +1,7 @@
 package com.example.movieapp.movie_popular_feature.domain.repository
 
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import com.example.movieapp.core.domain.model.Movie
-import kotlinx.coroutines.flow.Flow
 
 interface MoviePopularRepository {
 
