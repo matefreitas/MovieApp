4d8ac43ea63f754437c7a157806083746c7cdea5
mateus <mateus.silvafreitas@gmail.com>
2024-12-19 15:03:37 -0300

    resolve empty imports

--- app/src/main/java/com/example/movieapp/search_movie_feature/domain/repository/MovieSearchRepository.kt
+++ app/src/main/java/com/example/movieapp/search_movie_feature/domain/repository/MovieSearchRepository.kt
@@ -1,11 +1,7 @@
 package com.example.movieapp.search_movie_feature.domain.repository
 
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import androidx.paging.PagingSource
-import com.example.movieapp.core.domain.model.Movie
 import com.example.movieapp.core.domain.model.MovieSearch
-import kotlinx.coroutines.flow.Flow
 
 interface MovieSearchRepository {
     fun getSearchMovies(query: String): PagingSource<Int, MovieSearch>
